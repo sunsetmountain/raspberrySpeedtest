cb0daefe432e81884bb9a98483c61976fed670a9
sunsetmountain <brandon@sunsetmountain.com>
2018-10-06 14:48:59 -0600

    Create checkSpeed.py

---
+++ checkSpeed.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import re
+import subprocess
+import json
+import time
+import socket
+import datetime
+import calendar
+from tendo import singleton
+
+me = singleton.SingleInstance() # will sys.exit(-1) if another instance of this script is already running
+
+dataDir = "/home/pi/speedtestResults"
+
+def mkEpoch(inputDatestamp, inputTimestamp):
+	inputDatestamp = inputDatestamp.replace("/", "-")
+	inputStr = inputDatestamp + " " + inputTimestamp
+
+	datetimeObj = datetime.datetime.strptime(inputStr, "%Y-%m-%d %H:%M:%S.%f")
+	epochVal = calendar.timegm(datetimeObj.timetuple())
+
+	return epochVal
+
+def main():
+  currentDate = datetime.datetime.utcnow().strftime('%Y-%m-%d')
+  currentTime = datetime.datetime.utcnow().strftime('%H:%M:%S')
+  response = subprocess.Popen('speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read()
+
+  ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
+  download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
+  upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
+
+  ping[0] = ping[0].replace(',', '.')
+  download[0] = download[0].replace(',', '.')
+  upload[0] = upload[0].replace(',', '.')
+  
+  wifiResponse = subprocess.Popen('iw dev wlan0 link', shell=True, stdout=subprocess.PIPE).stdout.read()
+  ssid = re.findall('SSID:\s(.*?)\s', response, re.MULTILINE)
+  freq = re.findall('freq:\s(.*?)\s', response, re.MULTILINE)
+  signal = re.findall('signal:\s(.*?)\s', response, re.MULTILINE)
+  bitrate = re.findall('tx bitrate:\s(.*?)\s', response, re.MULTILINE)
+
+  ssid[0] = ssid[0].replace(',', '.')
+  freq[0] = freq[0].replace(',', '.')
+  signal[0] = signal[0].replace(',', '.')
+  bitrate[0] = bitrate[0].replace(',', '.')
+      
+  try:
+    if os.stat('/home/pi/speedtestResults/speedtest.csv').st_size == 0:
+        print 'Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s),SSID,Frequency,Signal,Bitrate'
+  except:
+    pass
+
+  print '{},{},{},{},{},{},{},{},{}'.format(currentDate, currentTime, ping[0], download[0], upload[0], ssid[0], freq[0], signal[0], bitrate[0])
+
+	except Exception,e:
+		print e
+		quit()
+
+if __name__ == '__main__':
+	main()
