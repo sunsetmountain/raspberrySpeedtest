c23f4aadd6c2d4ce5870f2f45cae61dddf86ad5c
sunsetmountain <brandon@sunsetmountain.com>
2018-10-06 14:28:53 -0600

    Update initialsoftware.sh

--- initialsoftware.sh
+++ initialsoftware.sh
@@ -1,5 +1,4 @@
-# Install all of the core software needed to make use of a SDR (software defined radio)
-# Based on instructions at https://www.rtl-sdr.com/video-tutorial-installing-gqrx-and-rtl-sdr-on-a-raspberry-pi/
+# Install all of the core software needed to perform repeated internet speed tests using a Raspberry Pi
 
 # Start at the beginning by going home.
 cd ~
@@ -8,3 +7,4 @@ cd ~
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install python-pip
 sudo pip install speedtest-cli
+sudo pip install tendo
