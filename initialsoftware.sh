1f976b1bbf5f0ecc00e32adb33db429645aaab18
sunsetmountain <brandon@sunsetmountain.com>
2018-10-06 16:30:21 -0600

    Update initialsoftware.sh

--- initialsoftware.sh
+++ initialsoftware.sh
@@ -8,3 +8,8 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install python-pip
 sudo pip install speedtest-cli
 sudo pip install tendo
+
+# Add startup commands to the end of .profile
+echo "# export GOOGLE_APPLICATION_CREDENTIALS='/home/pi/FILENAME.json'"
+echo "python /home/pi/raspberrySpeedtest/checkSpeed.py &" >> /home/pi/.profile
+echo "python /home/pi/raspberrySpeedtest/batchGCS.py &" >> /home/pi/.profile
