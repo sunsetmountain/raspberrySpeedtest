aa6cb627ccfa521d82312dce6de870ce43ec831b
sunsetmountain <brandon@sunsetmountain.com>
2018-10-08 18:02:51 -0600

    Update initialsoftware.sh

--- initialsoftware.sh
+++ initialsoftware.sh
@@ -19,6 +19,6 @@ cd ~/raspberrySpeedtest
 mkdir speedtestResults
 
 # Add startup commands to the end of .profile
-echo "# export GOOGLE_APPLICATION_CREDENTIALS='/home/pi/FILENAME.json'"
-echo "python /home/pi/raspberrySpeedtest/checkSpeed.py &" >> /home/pi/.profile
-echo "# python /home/pi/raspberrySpeedtest/batchGCS.py &" >> /home/pi/.profile
+echo "# export GOOGLE_APPLICATION_CREDENTIALS='/home/pi/FILENAME.json'" >> /home/pi/.profile
+echo "(sleep 15 && python /home/pi/raspberrySpeedtest/checkSpeed.py) &" >> /home/pi/.profile
+echo "# (sleep 20 && python /home/pi/raspberrySpeedtest/batchGCS.py) &" >> /home/pi/.profile
